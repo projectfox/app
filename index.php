fab0843052830db48273c6ab56edce324d559387
projectfox <sam.storbacka@hotmail.com>
2018-01-16 12:30:46 +0200

    Added loggout function and escaped some string

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 session_start();
 $title = "välkommen";
 include "includes/header.php";
-
  ?>
+<?php if ($_SESSION['username']): ?>
 <h1>välkommen <?php echo $_SESSION['$username']; ?></h1>
+<a href="logout.php"> Logga ut <?php echo $_SESSION['username']; ?></a>
 
+<?php else : ?>
+<h1>Inloggning upptäcktes inte</h1>
+<?php endif; ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -8,6 +8,9 @@
    $username = $_POST['username'];
    $password = $_POST['password'];
 
+   $username = mysqli_real_escape_string($connection, $username);
+   $username = mysqli_real_escape_string($connection, $password);
+
    $query = "SELECT * FROM  users WHERE username = '{$username}' ";
    $select_user_query = mysqli_query($connection, $query);
 
